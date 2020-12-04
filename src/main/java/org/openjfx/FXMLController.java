ebbe6ba8a56ed5ebb11e31ef5837b3f476808f3d
Taku3939 <sakisaki200012@gmail.com>
2020-12-05 02:44:23 +0900

    modify environment settings

--- src/main/java/org/openjfx/FXMLController.java
+++ src/main/java/org/openjfx/FXMLController.java
@@ -8,14 +8,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
-
+    
     @FXML
     private Label label;
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
-    }
+    }    
 }
--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -6,23 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
-
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-            var cl = getClass().getClassLoader();
-            Parent root = FXMLLoader.load(Objects.requireNonNull(cl.getResource("scene.fxml")));
-
-            Scene scene = new Scene(root);
-            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
-            stage.setTitle("JavaFX and Gradle");
-            stage.setScene(scene);
-            stage.show();
-
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
