dc1dc2f8a9097720e2a712d89921c658d61f478c
Taku3939 <sakisaki200012@gmail.com>
2021-01-04 20:52:19 +0900

    remove unneed code

--- src/main/java/game/view/state/CharacterState.java
+++ src/main/java/game/view/state/CharacterState.java
@@ -1,10 +1,4 @@
 package game.view.state;
 
-import com.taku.util.model.Vector2;
-import game.model.VCharacter;
-import javafx.scene.canvas.GraphicsContext;
-
 public class CharacterState {
-    public VCharacter c1 = new VCharacter();
-    public VCharacter c2 = new VCharacter();
 }
