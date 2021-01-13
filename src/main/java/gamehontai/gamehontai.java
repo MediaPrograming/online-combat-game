4db0220107354f9af5a217e4f02bd404b428ae01
AYATYA2 <bf1@outlook.jp>
2021-01-13 21:22:06 +0900

    message

--- src/main/java/gamehontai/gamehontai.java
+++ src/main/java/gamehontai/gamehontai.java
@@ -30,7 +30,7 @@ class phisicsobject extends Rectangle {
         setY((int)(getY()+vy));
     }
     public  void vector(double x,double y){
-        vx=x;vy=y;
+        vx=vx+x;vy=vy+y;
     }
     public void fall(){
         if(!randed){vy+=0.1;}
@@ -60,7 +60,8 @@ class phisicsobject extends Rectangle {
 
 class charactor extends phisicsobject{
     protected boolean a=false,s=false,d=false,w=false,jumped=false,atk=false;
-    protected phisicsobject attack;
+    protected phisicsobject attack;//あくまでattackは攻撃判定なので描写とずれるかも
+    protected int timetomove=0/*0の時キー入力を受け付ける*/,HP=100;
     charactor(double x,double y,double w,double h){
         super(x,y,w,h);
     }
@@ -69,26 +70,85 @@ class charactor extends phisicsobject{
         attack=a;
     }
     public void keycheck(){
-        if(a&&vx>-5){
-            vx+=-5;
-        }else if(a&&vx<=-5){
-            vx=-10;
-        }else if(!a&&vx>=-10&&vx<=0){
-            vx=0;//攻撃とかで吹っ飛ばす時は｜vx｜>10になるようにして
-        }
-        if(d&&vx<5){
-            vx+=5;
-        }else if(d&&vx>5){
-            vx=10;
-        }else if(!d&&vx<=10&&vx>=0){
-            vx=0;
-        }
-        if(w&&randed){
-            vector(0,-5);
+        if(timetomove==0){
+            if(atk){//攻撃範囲と硬直時間を設定
+                if(a){
+                    attack.setX(this.getX()-this.getWidth()/2);
+                    attack.setY(this.getY()+this.getHeight()/4);
+                    attack.setWidth(this.getWidth()/2);
+                    attack.setHeight(this.getHeight()/2);
+                }else if(d){
+                    attack.setX(this.getX()+this.getWidth());
+                    attack.setY(this.getY()+this.getHeight()/4);
+                    attack.setWidth(this.getWidth()/2);
+                    attack.setHeight(this.getHeight()/2);
+                }else if(w){
+                    attack.setX(this.getX()+this.getWidth()/4);
+                    attack.setY(this.getY()-this.getHeight()/2);
+                    attack.setWidth(this.getWidth()/2);
+                    attack.setHeight(this.getHeight()/2);
+                }else if(s){
+                    attack.setX(this.getX()-this.getWidth()/4);
+                    attack.setY(this.getY()+3*this.getHeight()/4);
+                    attack.setWidth(3*this.getWidth()/2);
+                    attack.setHeight(this.getHeight()/4);
+                }else{
 
+                }
+                attack.setVisible(true);
+                atk=a=s=d=w=false;
+                timetomove=60;
+            }else {
+                attack.setVisible(false);
+                attack.setX(-1);
+                attack.setY(-1);
+                attack.setWidth(1);
+                attack.setHeight(1);
+            }
+            if(a&&vx>-5){
+                vx+=-5;
+            }else if(a&&vx<=-5){
+                vx=-10;
+            }else if(!a&&vx<=0){
+                vx=(int)(vx/5.0);
+            }
+            if(d&&vx<5){
+                vx+=5;
+            }else if(d&&vx>=5){
+                vx=10;
+            }else if(!d&&vx>=0){
+                vx=(int)(vx/5.0);
+            }
+            if(w&&randed){
+                vector(0,-5);
+            }
+
+        }else{
+            timetomove--;
+            if(attack.getX()!=-1){
+                attack.setVx(this.getVx());
+                attack.setVy(this.getVy());
+                attack.move();
+            }
         }
     }
-
+    public int getTimetomove(){
+        return timetomove;
+    }
+    public int setTimetomove(int t){
+        timetomove=t;
+        return t;
+    }
+    public boolean getatk(){
+        return  atk;
+    }
+    public int Damage(int d){
+        HP=HP-d;
+        return HP;
+    }
+    public int getHP(){
+        return HP;
+    }
     @Override
     public boolean setRanded(boolean b) {
         if(b){
@@ -121,7 +181,7 @@ class charactor extends phisicsobject{
                // System.out.println("w is pressed");
                 w=true;
                 break;
-            case " ":
+            case "K":
                 atk=true;
                 break;
         }
@@ -144,6 +204,9 @@ class charactor extends phisicsobject{
                 w=false;
  //               System.out.println("w is released");
                 break;
+            case "K":
+                atk=false;
+                break;
         }
     }
 }
@@ -151,7 +214,7 @@ class charactor extends phisicsobject{
 public class gamehontai extends Application
 {
     public void charasyoutotu(phisicsobject a,phisicsobject b) {
-        if (a.intersects(b.getX() + b.getVx() - 1 - a.getVx(), b.getY() + b.getVy() - 1 - a.getVy(), b.getWidth() + 1, b.getHeight() + 1)) {
+        if (a.intersects(b.getX() + b.getVx() - 1 - a.getVx(), b.getY() + b.getVy() - 1 - a.getVy(), b.getWidth() + 2, b.getHeight() + 2)) {
             if (a.intersects(b.getX() + b.getVx() - 1 - a.getVx(), b.getY() - 1, b.getWidth() + 1, b.getHeight() + 1)) {
                 if (a.getX() < b.getX()) {
                     //a.setVx((b.getX()-a.getWidth()-2-a.getX()));
@@ -176,7 +239,7 @@ public class gamehontai extends Application
                     }
                 }
             }
-            if (a.intersects(b.getX() - 1, b.getY() + b.getVy() - 1 - a.getVy(), b.getWidth() + 1, b.getHeight() + 1)) {
+            if (a.intersects(b.getX() - 1, b.getY() + b.getVy() - 1 - a.getVy(), b.getWidth() + 2, b.getHeight() + 2)) {
                 if (a.getY() < b.getY()) {
                     if (a.getVy() <= 0) {
                         b.setVy((int)(a.getY() + a.getVy() + a.getHeight() + 2 - b.getY()));
@@ -205,6 +268,71 @@ public class gamehontai extends Application
 
         }
     }
+
+    //charactor aが攻撃を食らったとき-1,bが食らったとき1を返す。どちらでもないとき0を返す。
+    public int is_attack_hit(charactor a,phisicsobject aattack,charactor b,phisicsobject battack){
+        if(aattack.intersects(battack.getX(),battack.getY(),battack.getWidth(),battack.getHeight())){
+            return 0;
+        }
+        if(aattack.intersects(b.getX()+b.getVx(),b.getY()+b.getVy(),b.getWidth(),b.getHeight())){
+            if(aattack.getX()== a.getX()+a.getWidth()/4){
+                b.vector(0,-10);
+            }else if(aattack.getX()== a.getX()-a.getWidth()/2){
+                b.vector(-5,-2);
+            } else if(aattack.getX()== a.getX()+a.getWidth()){
+                b.vector(5,-2);
+            }else if(aattack.getX()== a.getX()-a.getWidth()/4){
+                if(b.getX()<=a.getX()){
+                    b.vector(-2,-2);
+                }else{
+                    b.vector(2,-2);
+                }
+            }
+            aattack.setVisible(false);
+            aattack.setY(-1);
+            aattack.setX(-1);
+            aattack.setWidth(1);
+            aattack.setHeight(1);
+            battack.setVisible(false);
+            battack.setY(-1);
+            battack.setX(-1);
+            battack.setWidth(1);
+            battack.setHeight(1);
+            b.setTimetomove(40);
+            return 1;
+        }
+        if(battack.intersects(a.getX()+a.getVx(),a.getY()+a.getVy(),a.getWidth(),a.getHeight())){
+            if(battack.getX()== b.getX()+b.getWidth()/4){
+                a.vector(0,-10);
+            }else if(battack.getX()== b.getX()-b.getWidth()/2){
+                a.vector(-5,-2);
+            }else if(battack.getX()== b.getX()+b.getWidth()){
+                a.vector(5,-2);
+            }else if(battack.getX()== b.getX()-b.getWidth()/4){
+                if(b.getX()<=a.getX()){
+                    a.vector(2,-2);
+                }else{
+                    a.vector(-2,-2);
+                }
+
+            }
+            aattack.setVisible(false);
+            aattack.setY(-1);
+            aattack.setX(-1);
+            aattack.setWidth(1);
+            aattack.setHeight(1);
+            a.setTimetomove(40);
+            battack.setVisible(false);
+            battack.setY(-1);
+            battack.setX(-1);
+            battack.setWidth(1);
+            battack.setHeight(1);
+            b.setTimetomove(40);
+            return -1;
+        }
+        return 0;
+    }
+
 
     public static void main(String[] args)
     {
@@ -243,28 +371,34 @@ public class gamehontai extends Application
                     }
                 }
         );
-        Timeline timer=new Timeline(new KeyFrame(Duration.millis(17),new EventHandler<ActionEvent>(){
-            int count=0;
-            public void handle(ActionEvent event) {
-                //毎フレームごとに呼び出すメゾット
+        Timeline batlletimer=new Timeline(new KeyFrame(Duration.millis(17),new EventHandler<ActionEvent>(){
+            int count=0;//敵を適当に動かすようなので消す。
+            public void handle(ActionEvent event) {                //毎フレームごとに呼び出す処理
                 chare.fall();
                 enemy.fall();
                 chare.keycheck();
-
                 if(count==0){
-                    enemy.keypressed("W");
-                    enemy.keyreleased("D");
+                    enemy.keypressed("K");
                     enemy.keypressed("A");
                 }
+              /*
                 if(count==20){
                     enemy.keyreleased("A");
                 }
                 if(count==40){
                     enemy.keypressed("D");
-                }
+                }敵の挙動確認用*/
                 count++;
-                count=count%60;
+                count=count%120;
                 enemy.keycheck();
+                is_attack_hit(chare,chareattack,enemy,enemyattack);
+                if(chare.getHP()<=0||enemy.getHP()<=0){
+                    if(chare.getHP()<=0){
+                        //chareの勝利アニメーションとか
+                    }  else {
+                        //enemyの勝利アニメーションとか
+                    }
+                }
                 charasyoutotu(chare,flore);
                 charasyoutotu(enemy,flore);
                 charasyoutotu(chare,enemy);
@@ -278,12 +412,12 @@ public class gamehontai extends Application
                 chare.move();
                 enemy.move();
                }
-        }));
-        timer.setCycleCount(Timeline.INDEFINITE);
-        timer.play();
+        }, new javafx.animation.KeyValue[]{}));
+        batlletimer.setCycleCount(Timeline.INDEFINITE);
+        batlletimer.play();
 
         theStage.setScene(scene);
-        theStage.setTitle("Hello, World!");
+        theStage.setTitle("Battle game");
         theStage.show();
 
 
