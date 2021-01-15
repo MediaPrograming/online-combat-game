c9d11088575a86b5a4e818765e428c8c478a064a
Vimii <vimii8003@gmail.com>
2021-01-15 17:55:24 +0900

    play deketa

--- src/main/java/Animation/animationHolder.java
+++ src/main/java/Animation/animationHolder.java
@@ -1,25 +1,19 @@
 package Animation;
 
-import game.config.Action;
 import game.config.Character;
 import game.config.PATH;
 import io.game.hub.positionHub.Behavior;
-import io.game.hub.positionHub.CharacterState;
-import io.game.hub.positionHub.State;
-import javafx.scene.image.Image;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.Hashtable;
 
 /*連番ImageをArrayListに保存しておくクラス*/
 public class animationHolder {
-    private Hashtable<String,Hashtable<Behavior,Image[][]>> table = new Hashtable<>();
-    private Hashtable<Behavior,Image[][]> gura = new Hashtable<>();
-    private Hashtable<Behavior,Image[][]> ame = new Hashtable<>();
-    private Hashtable<Behavior,Image[][]> ina = new Hashtable<>();
-    private Hashtable<Behavior,Image[][]> calli = new Hashtable<>();
-    private Hashtable<Behavior,Image[][]> kiara = new Hashtable<>();
+    private static Hashtable<String,Hashtable<Behavior,Animation>> table = new Hashtable<>();
+    private static Hashtable<Behavior,Animation> gura = new Hashtable<>();
+    private static Hashtable<Behavior,Animation> ame = new Hashtable<>();
+    private static Hashtable<Behavior,Animation> ina = new Hashtable<>();
+    private static Hashtable<Behavior,Animation> calli = new Hashtable<>();
+    private static Hashtable<Behavior,Animation> kiara = new Hashtable<>();
     public animationHolder(){
         table.put(Character.Gura,gura);
         table.put(Character.Ame,ame);
@@ -29,24 +23,24 @@ public class animationHolder {
         this.addAllAnimations();
     }
 
-    private void addAllAnimations() {
-        this.addAnimation(Character.Gura, Behavior.NORMAL ,PATH.Gura_Normal,128,128,2,1);
-        this.addAnimation(Character.Kiara,Behavior.NORMAL,PATH.Kiara_Normal,200,200,5,5);
+    private static void addAllAnimations() {
+        animationHolder.addAnimation(Character.Gura, Behavior.NORMAL ,PATH.Gura_Normal,128,128,2,1,2,true);
+        animationHolder.addAnimation(Character.Kiara,Behavior.NORMAL,PATH.Kiara_Normal,200,200,5,5,3,true);
     }
 
 //    public ArrayList<Image[][]> getAnimations(){
 //        return movingImgs;
 //    }
 
-    public Image[][] getAnimation(String chara, Behavior key){
+    public static Animation getAnimation(String chara, Behavior key){
         var img = table.get(chara).get(key);
         if(img == null) System.out.println("キーが登録されていません");
         return img;
     }
 //    public int getImgNum(){return movingImgs.size();}
 
-    public void addAnimation(String chara,Behavior key,String url,int pixelX,int pixelY,int numX,int numY){
+    public static void addAnimation(String chara,Behavior key,String url,int pixelX,int pixelY,int numX,int numY,int speed,boolean loop){
         getAnimationFromImg im = new getAnimationFromImg(url, pixelX, pixelY, numX, numY);
-        table.get(chara).put(key,im.createAnimation());
+        table.get(chara).put(key,new Animation(im.createAnimation(),speed,loop));
     }
 }
--- src/main/java/game/view/panel/UoPanel.java
+++ src/main/java/game/view/panel/UoPanel.java
@@ -1,23 +1,17 @@
 package game.view.panel;
+import Animation.Animation;
 import Animation.animationHolder;
-import Animation.animationKeyHolder;
 import Animation.playAnimation;
 import com.taku.util.flux.view.BasePanel;
-import com.taku.util.model.Unit;
-import game.config.Action;
 import game.config.Character;
 import game.config.PATH;
-import game.store.StoreManager;
 import game.util.Time;
-import game.view.action.UIEvent;
 import game.view.container.CharacterContainer;
 import game.view.service.ICharacter;
-import game.view.state.CharaState;
 import io.game.hub.positionHub.Behavior;
 import io.game.hub.positionHub.CharacterState;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import java.awt.*;
@@ -25,10 +19,9 @@ import javafx.scene.image.Image;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
@@ -41,26 +34,19 @@ public class UoPanel extends BasePanel<CharacterState, ICharacter> implements In
     double initTime;
 
     CharacterState state;
+    private ArrayList<playAnimation> player;
 
     //test
     private int uouo;
     private Text text;
-    private animationHolder anim;
 
-    private playAnimation player1;
-
-    private Image[][] Gura,Kiara;
-    @FXML
-    private ImageView imgv;
-
-    private Image clip;
-    private WritableImage wclip;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         new CharacterContainer(this);
+        new animationHolder();
 
         var props = this.getProps();
         state = this.getState();
@@ -71,25 +57,15 @@ public class UoPanel extends BasePanel<CharacterState, ICharacter> implements In
         initTime = Time.Instance.getTotalTime();
         uouo = 0;
 
-//        this.draw();
         text = new Text (String.valueOf(initTime));
         text.setStroke(Color.BLACK);
 
+        player = new ArrayList<>();
 
-//        imgv = new ImageView(new File(PATH.Gura).toURI().toString());
-//
-//        Gura = new Image[5][2];
+        player.add(new playAnimation(1,Character.Gura));
+        player.add(new playAnimation(2,Character.Kiara));
 
-        anim = new animationHolder();
-        anim.addAnimation(Character.Gura, Behavior.NORMAL,PATH.Gura_Normal,128,128,2,1);
-        anim.addAnimation(Character.Kiara,Behavior.NORMAL,PATH.Kiara_Normal,200,200,5,5);
-        Gura = anim.getAnimation(Character.Gura,Behavior.NORMAL);
-        Kiara = anim.getAnimation(Character.Kiara,Behavior.NORMAL);
 
-        player1 = new playAnimation(1,Character.Gura);
-
-//        clip = new Image(new File(PATH.Gura).toURI().toString());
-//        Gura[0][0] = new WritableImage(clip.getPixelReader(),0,0 , 120, 120);
 
     }
 
@@ -102,11 +78,8 @@ public class UoPanel extends BasePanel<CharacterState, ICharacter> implements In
         gc.fillRect(uouo, 50, 150, 150);
         gc.strokeOval(uouo, 300, 50, 50);
 
-//        imgv.setImage(earth);
-//        gc.drawImage(Gura[((int)Time.Instance.getTotalTime())%2][0], uouo,400,250,250);
-        gc.drawImage(Kiara[((int)(Time.Instance.getTotalTime()*5))%5][((int)Time.Instance.getTotalTime())%5], uouo,200,400,400);
-
-        gc.drawImage(player1.playAnimation(state), uouo,400,250,250);
+        gc.drawImage(player.get(1).playAnimation(state), uouo,400,400,400);
+        gc.drawImage(player.get(0).playAnimation(state), uouo,200,250,250);
 
         gc.setFill(Color.RED);
         gc.fillText(""+text, 300, 100);
@@ -117,7 +90,6 @@ public class UoPanel extends BasePanel<CharacterState, ICharacter> implements In
     public void EveryFrameUpdate(){
         System.out.println("fps"+Time.Instance.getFrameRate()+"uouo"+uouo);
         uouo++;
-        gc.setStroke(Color.RED);
 //        gc.strokeText("Total Time : " + (Time.Instance.getTotalTime()-initTime)+ "\n delta Time : " + Time.Instance.getDeltaTime(), 250, 200);
 
         text.setText(""+uouo+"init"+initTime);
