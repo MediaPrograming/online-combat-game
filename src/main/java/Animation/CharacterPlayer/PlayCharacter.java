655655fd6479fa1fb82c33a621ddce5843e50f83
Vimii <vimii8003@gmail.com>
2021-01-22 02:30:57 +0900

    Ugokuzo

--- src/main/java/Animation/CharacterPlayer/PlayCharacter.java
+++ src/main/java/Animation/CharacterPlayer/PlayCharacter.java
@@ -4,9 +4,12 @@ import Animation.CharaAnimationPlayer;
 import Animation.EffectPlayer.*;
 import Animation.CharaAnimationPlayer;
 import game.config.Character;
+import io.game.hub.positionHub.Behavior;
 import io.game.hub.positionHub.CharacterState;
 import io.game.hub.positionHub.Direction;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class PlayCharacter {
     protected double width,height;
@@ -38,7 +41,8 @@ public class PlayCharacter {
     public void setWidth(double width){ this.width = width; }
     public void setHeight(double height){ this.height = height; }
     public void setPositionFromState(){
-        X = state.getX();
+        if(state.getDirection() == Direction.RIGHT) X = state.getX();
+        else X = state.getX() - this.width;
         Y = state.getY();
     }
 
@@ -52,12 +56,50 @@ public class PlayCharacter {
     }
 
     public void play(){
-        //test
-        X = Y = 400;
-
-        setPositionFromState();
         adjustDirection(state);
+        setPositionFromState();
         gc.drawImage(player.play(state),X,Y,width,height);
+
+        //test
+        ShowStatus();
+    }
+
+    public void ShowStatus(){
+        gc.setFill(Color.BLUE);
+        gc.setFont(Font.font(20));
+        switch (this.state.getBehavior()) {
+            case NORMAL:
+                gc.fillText("STATE:NORMAL",X,Y+100,300);
+                break;
+            case RUN:
+                gc.fillText("STATE:RUN",X,Y+100,300);
+                break;
+            case JUMP:
+                gc.fillText("STATE:JUMP",X,Y+100,300);
+                break;
+            case SQUAT:
+                gc.fillText("STATE:SQUAT",X,Y+100,300);
+                break;
+            case DAMAGE:
+                gc.fillText("STATE:DAMAGE",X,Y+100,300);
+                break;
+            case ATTACK1:
+                gc.fillText("STATE:ATTACK1",X,Y+100,300);
+                break;
+            case ATTACK2:
+                gc.fillText("STATE:ATTACK2",X,Y+100,300);
+                break;
+            case ATTACK3:
+                gc.fillText("STATE:ATTACK3",X,Y+100,300);
+                break;
+            case ATTACK4:
+                gc.fillText("STATE:ATTACK4",X,Y+100,300);
+                break;
+            case DEFENCE:
+                gc.fillText("STATE:DEFENCE",X,Y+100,300);
+                break;
+        }
+
     }
 
 }
