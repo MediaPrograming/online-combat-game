dde327f0dc5ca2137af0272d7bf5406a1ac28163
Vimii <vimii8003@gmail.com>
2021-01-29 16:59:20 +0900

    uo

--- src/main/java/game/config/CharaData/Gura.java
+++ src/main/java/game/config/CharaData/Gura.java
@@ -7,4 +7,7 @@ public final class Gura {
     public static final int Defence = 20;
     public static final int width = 100;
     public static final int height = 200;
+
+    public static final int TRIDENT_W = 200;
+    public static final int TRIDENT_H = 50;
 }
