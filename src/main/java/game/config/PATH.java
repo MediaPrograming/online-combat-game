08eb2e1c2844d32055d04f8e06344fcd960c933d
Vimii <vimii8003@gmail.com>
2021-02-02 00:59:10 +0900

    \->/

--- src/main/java/game/config/PATH.java
+++ src/main/java/game/config/PATH.java
@@ -7,38 +7,33 @@ import java.io.File;
 
 public final class PATH {
     public static final String root = new File(".").getAbsolutePath();
-    public static final String img = "..\\img";
-    public static final String sounds = root + "\\src\\main\\resources\\game\\sounds";
-    public static final String Back = img + "\\Back.jpg";
-    public static final String Floor = img + "\\floor.png";
-    public static final String Kusa = img + "\\kusa.png";
-
-    public static final String HIT = img + "\\hiteffect -Sheet.png";
-
-    public static final String Gura_Normal = img + "\\Gura\\Gura-02-Sheet.png";
-    public static final String Gura_Run = img + "\\Gura\\Gura-03-Sheet.png";
-    public static final String Gura_Attack1 = img + "\\Gura\\Gura-04-Sheet.png";
-    public static final String Kiara_Normal = img + "\\Kiara\\小鳥遊キアラ-Sheet - .png";
-    public static final String Kiara_Run = img + "\\Kiara\\小鳥遊キアラrun-Sheet.png";
-    public static final String Kiara_Attack1 = img + "\\Kiara\\小鳥遊キアラyokoattack-Sheet.png";
-    public static final String Kiara_Attack3 = img + "\\Kiara\\小鳥遊キアラupperattack-Sheet.png";
-    public static final String Kiara_Attack4 = img +"\\Kiara\\小鳥遊キアラeat-Sheet.png";
-    public static final String Kiara_Jump=img +"\\Kiara\\小鳥遊キアラjump-Sheet.png";
-    public static final String Kiara_Damage= img +"\\Kiara\\小鳥遊キアラdamage-Sheet.png";
-
-    public static final String Amelia_Normal = img + "\\Amelia\\Amerial-Nornal-01.png";
-    public static final String Amelia_Attack1 = img + "\\Amelia\\Amerial-Attack-01.png";
-    public static final String Amelia_Attack3 = img + "\\Amelia\\Amerial-Attack-Upper-01.png";
-    public static final String Amelia_Attack4 = img + "\\Amelia\\Amerial-Attack-Under-01.png";
-    public static final String Amelia_Run = img + "\\Amelia\\Amerial-Walk-01.png";
-    public static final String Amelia_Atk_obj = img + "\\Amelia\\Amelia-Atk-obj.png";
-    public static final String Amelia_Damaged = img + "\\Amelia\\Amerial-Damaged-01.png";
-    public static final String BattleBGM = sounds + "\\BGM\\Tanukichi.wav";
-    public static final String HomeBGM = sounds + "\\BGM\\のんきな日常.wav";
-    public static final String FinishBGM = sounds + "\\BGM\\Let's!.wav";
-
-    public static final String TRIDENT = img + "\\Gura\\Guratrydent-Sheet.png";
-
-    public static final String Effect_HIT = sounds + "\\hit.wav";
-    public static final String Hizyousyoku =img +"\\hizyousyoku-Sheet .png";
+    public static final String img = "../img";
+    public static final String sounds = root + "/src/main/resources/game/sounds";
+    public static final String Back = img + "/Back.jpg";
+    public static final String Floor = img + "/floor.png";
+    public static final String Kusa = img + "/kusa.png";
+    public static final String HIT = img + "/hiteffect -Sheet.png";
+    public static final String Gura_Normal = img + "/Gura/Gura-02-Sheet.png";
+    public static final String Gura_Run = img + "/Gura/Gura-03-Sheet.png";
+    public static final String Gura_Attack1 = img + "/Gura/Gura-04-Sheet.png";
+    public static final String Kiara_Normal = img + "/Kiara/小鳥遊キアラ-Sheet - .png";
+    public static final String Kiara_Run = img + "/Kiara/小鳥遊キアラrun-Sheet.png";
+    public static final String Kiara_Attack1 = img + "/Kiara/小鳥遊キアラyokoattack-Sheet.png";
+    public static final String Kiara_Attack3 = img + "/Kiara/小鳥遊キアラupperattack-Sheet.png";
+    public static final String Kiara_Attack4 = img +"/Kiara/小鳥遊キアラeat-Sheet.png";
+    public static final String Kiara_Jump=img +"/Kiara/小鳥遊キアラjump-Sheet.png";
+    public static final String Kiara_Damage= img +"/Kiara/小鳥遊キアラdamage-Sheet.png";
+    public static final String Amelia_Normal = img + "/Amelia/Amerial-Nornal-01.png";
+    public static final String Amelia_Attack1 = img + "/Amelia/Amerial-Attack-01.png";
+    public static final String Amelia_Attack3 = img + "/Amelia/Amerial-Attack-Upper-01.png";
+    public static final String Amelia_Attack4 = img + "/Amelia/Amerial-Attack-Under-01.png";
+    public static final String Amelia_Run = img + "/Amelia/Amerial-Walk-01.png";
+    public static final String Amelia_Atk_obj = img + "/Amelia/Amelia-Atk-obj.png";
+    public static final String Amelia_Damaged = img + "/Amelia/Amerial-Damaged-01.png";
+    public static final String BattleBGM = sounds + "/BGM/Tanukichi.wav";
+    public static final String HomeBGM = sounds + "/BGM/のんきな日常.wav";
+    public static final String FinishBGM = sounds + "/BGM/Let's!.wav";
+    public static final String TRIDENT = img + "/Gura/Guratrydent-Sheet.png";
+    public static final String Effect_HIT = sounds + "/hit.wav";
+    public static final String Hizyousyoku =img +"/hizyousyoku-Sheet .png";
 }
